fc4cb65abb185d4f1672e6a99b89af001dfa2c83
Pulkit Tripathi <pulkit.tripathi@outlook.com>
2019-04-10 22:40:12 -0400

    formatted feed display, added pagination and redirection to source url

--- webui/src/app/home/home.component.ts
+++ webui/src/app/home/home.component.ts
@@ -1,6 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
+
 import { FeedAggregateService } from '../feed-aggregate.service';
 import { FeedAggregate } from '../feed-aggregate';
+import { FeedMessage } from '../feed-message';
+
+const testAggregateList = [
+  { title: 'Title 1', description: 'Description 1', link: 'http://podcasts.joerogan.net/feed', author: 'Author 1', guid: 'guid 1' },
+  { title: 'Title 2', description: 'Description 2', link: 'http://rss.nytimes.com/services/xml/rss/nyt/World.xml', author: 'Author 2', guid: 'guid 2' }
+];
 
 @Component({
   selector: 'app-home',
@@ -10,21 +18,38 @@ import { FeedAggregate } from '../feed-aggregate';
 export class HomeComponent implements OnInit {
 
   feedAggregate: FeedAggregate = new FeedAggregate();
+  // tableColumns = ['title', 'description'];
+  tableColumns = ['feed'];
+  dataSource = new MatTableDataSource<FeedMessage>(testAggregateList);
 
-  constructor(private faService: FeedAggregateService) { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private aggregateService: FeedAggregateService) { }
 
   ngOnInit() {
-    this.faService.getFeedAggregate()
+    this.dataSource.paginator = this.paginator;
+    this.aggregateService.getFeedAggregate()
       .toPromise()
       .then(aggregate => {
         this.feedAggregate.aggregateList = aggregate;
+        this.dataSource.data = aggregate;
         console.log('this.feedAggregate:');
         console.log(this.feedAggregate);
       });
-    // this.feedAggregate.aggregateList = [
-    //   { title: 'Title 1', description: 'Description 1', link: 'Link 1', author: 'Author 1', guid: 'guid 1' },
-    //   { title: 'Title 2', description: 'Description 2', link: 'Link 2', author: 'Author 2', guid: 'guid 2' }
-    // ];
+  }
+
+  redirectToSource(url: string): void  {
+    window.open(url, '_blank');
+  }
+
+  getDomainFromUrl(url: string) {
+    // const r = /:\/\/(.[^/]+)/;
+    if (!(url.length === 0)) {
+      const domain = url.match(/:\/\/(.[^/]+)/);
+      if (!(domain == null)) {
+        return domain[1];
+      }
+    }
   }
 
 }
