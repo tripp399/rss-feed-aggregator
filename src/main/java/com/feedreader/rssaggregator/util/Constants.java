94444a90befe90543fe9a4b04ecc95f162f5983a
Nishtha Shrivastav <nshrivastav@ufl.edu>
2019-04-17 17:36:29 -0400

    Changes in tests and opml parser

--- src/main/java/com/feedreader/rssaggregator/util/Constants.java
+++ src/main/java/com/feedreader/rssaggregator/util/Constants.java
@@ -7,6 +7,4 @@ public class Constants {
 	public static String URL3= "https://gist.githubusercontent.com/daveaglick/91146698402e3e1409bbe2b7a3ab96d7/raw/eaf1d76afbe762807ea6c789f84f4821778f9b64/feedly.opml";
 	public static String URL4= "https://gist.githubusercontent.com/spaghettidba/4b5a6d47bb61a16456c5400f4bc1cd7a/raw/2ef76b56cc9ec2687ca6610e003bb2a4d5dc0ae1/ForumSurfer.OPML";
   public static String URL5="https://raw.githubusercontent.com/kilimchoi/engineering-blogs/master/engineering_blogs.opml";
-  public static String URL6="https://github.com/scripting/feedsForJournalists/blob/master/list.opml";
-  
 }
